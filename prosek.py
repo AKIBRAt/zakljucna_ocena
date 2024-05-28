6fd25c7aceb2d7188e780572635af61ce366b0fe
AKIBRAt <aleksej.rodic11@gmail.com>
2024-05-28 16:30:38 +0200

    prosek

---
+++ prosek.py
@@ -0,0 +1,8 @@
+ocena1=int(input("unesi ocenu "))
+ocena2=int(input("unesi ocenu "))
+ocena3=int(input("unesi ocenu "))
+ocena4=int(input("unesi ocenu "))
+ocena5=int(input("unesi ocenu "))
+zbirSvih=ocena1+ocena2+ocena3+ocena4+ocena5
+prosek=zbirSvih/5
+print(prosek)
